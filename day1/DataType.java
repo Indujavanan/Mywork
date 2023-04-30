9b9f10cece2d15844092fc63a9c89669ef61ccee
Indujavanan <132189556+Indujavanan@users.noreply.github.com>
2023-04-30 13:40:58 +0530

    Add files via upload
    Day1

---
+++ day1/DataType.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+
+		int roleNo = 123;
+		String name = "Induja";
+		boolean havePresent = true;
+		System.out.println(roleNo+" "+name+" "+havePresent+" ");
+
+	}
+
+}
---
+++ day1/Fibbinocci.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Fibbinocci { 
+	public static void main(String[] args) {
+		
+	int firstnum=0;
+	int secnum=1;
+	int sum=0;
+	System.out.println(firstnum);
+	for(int i=1;i<=11;i++) {
+		 sum=firstnum+secnum;
+		 firstnum=secnum;
+		 secnum=sum;
+		 System.out.println(sum);
+	}
+	
+		
+	}   
+
+}
